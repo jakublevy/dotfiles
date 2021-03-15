7c359fe8e864fd4515ff267613ef26a1596ff8ec
Jakub Levý <jakub.levy15@gmail.com>
2021-03-15 11:29:11 +0100

    Anki auto suspend cards repaired

--- .local/share/Anki2/addons21/1565222544/__init__.py
+++ .local/share/Anki2/addons21/1565222544/__init__.py
@@ -1,34 +1,35 @@
+from anki import hooks
 from anki.hooks import wrap
 from aqt.utils import tooltip
 from anki.schedv2 import Scheduler
 from aqt import mw
+from aqt.utils import showWarning
 
 config = mw.addonManager.getConfig(__name__)
 # config["burypoint"] #    5: automatically buries learning/relearning queue cards if you fail them this many times in a single day. does not add any tags.
-# config["killpoint"] #  140: suspends cards if you review them successfully and the resulting interval is at least this value in days. adds a "auto-suspended" tag.
+# config["suspendpoint"] #  140: suspends cards if you review them successfully and the resulting interval is at least this value in days. adds a config["suspendTag"] tag.
 
-# burying and killing can be disabled for specific cards by adding special tags
-# "No::Burry" to disable burying
-# "No::Suspend" to disable killing
+# burying and suspending can be disabled for specific cards by adding special tags
+# config["disableBuryTag"] to disable burying
+# config["disableSuspendTag"] to disable suspending
 
 to_bury = []
 
-def checkKill(self, card, ease, early, _old):
-    ret = _old(self, card, ease, early)
+def checkSuspend(card, ease, early):
     if early:
-        return ret
+        return
     
     note = card.note()
-    if note.hasTag("No::Suspend"):
-        return ret
+    if note.hasTag(config["disableSuspendTag"]):
+        return
     
-    if card.ivl >= config["killpoint"]:
-        self.suspendCards([card.id])
-        note.addTag("auto-suspended")
+    if card.ivl >= config["suspendpoint"]:
+        card.queue = -1 #suspend card
+        note.addTag(config["suspendTag"])
         note.flush()
-        tooltip(_("Card automatically auto-suspended and tagged due to high maturity"))
-    return ret
-
+        card.flush()
+        tooltip(_("Card automatically suspended and tagged due to high maturity"))
+    
 def checkBury(self, card, ease, _old):
     global to_bury
     ret = _old(self, card, ease)
@@ -36,10 +37,11 @@ def checkBury(self, card, ease, _old):
         return ret
     
     note = card.note()
-    if note.hasTag("No::Burry"):
+    if note.hasTag(config["disableBuryTag"]):
         return ret
     
     count_relapses_today = self.col.db.scalar("select count() from revlog where ease = 1 and type = 2 and cid = ? and id > ?", card.id, (self.col.sched.dayCutoff-86400)*1000)
+    showWarning(count_relapses_today)
     if count_relapses_today >= config["burypoint"]:
         tooltip(_("Card automatically buried until tomorrow"))
         to_bury += [card.id]
@@ -55,6 +57,7 @@ def handleBury(self, card, ease, _old):
     return ret
 
 
-Scheduler._rescheduleRev = wrap(Scheduler._rescheduleRev, checkKill, "around")
+hooks.schedv2_did_answer_review_card.append(checkSuspend)
+
 Scheduler._answerLrnCard = wrap(Scheduler._answerLrnCard, checkBury, "around")
 Scheduler.answerCard = wrap(Scheduler.answerCard, handleBury, "around")
---
+++ .local/share/Anki2/addons21/9/__init__.py
@@ -0,0 +1,7 @@
+import os
+from anki import hooks
+from aqt import guihooks
+from aqt.utils import showWarning
+
+thisScriptDir = os.path.dirname(__file__)
+
